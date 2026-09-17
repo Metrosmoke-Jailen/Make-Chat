de1ef87b4894ae899f992cb9ea682b07d08f9148
Jailen-Truitt <jailen.johnson-truitt@students.dominican.edu>
2026-09-17 12:24:47 -0700

    project init

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-
 const express = require('express');
 const app = express();
-//Socket.io has to use the http server
+
+// Socket.io has to use the HTTP server
 const server = require('http').Server(app);
 
-//Express View Engine for Handlebars
-const exphbs  = require('express-handlebars');
+// Express View Engine for Handlebars
+const exphbs = require('express-handlebars');
+
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
   res.render('index.handlebars');
-})
+});
 
 server.listen('3000', () => {
   console.log('Server listening on Port 3000');
-})
+});
